f49988ee7493d405a571366f9fb8ce9e961fb8f4
Darcie <darciewalsh@hotmail.com>
2018-11-04 17:54:34 +0000

    add and fix issue with Enzyme, needed to create setupTest file, test now passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+/* global it, expect */
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import "./setupTests"
+
+it('App renders without crashing', () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
